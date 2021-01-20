e8c0aa0bd0bb515a04fe63d3bad64ade64ec5e0a
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-01-21 00:11:49 +0100

    css very very hard yes

--- app/app.js
+++ app/app.js
@@ -2,34 +2,32 @@ const inputField = document.querySelector(".input-city");
 const submitButton = document.querySelector(".submit-button");
 const cityOfChoice = document.querySelector(".city-of-choice");
 const weatherOfCity = document.querySelector(".weather-of-city");
+const weatherDesc = document.querySelector(".weather-description");
 const temperatureOfCity = document.querySelector(".temperature-of-city");
 
-import { OW_API_KEY, GOOGLE_API_KEY} from "./API_key.js";
+import { OW_API_KEY } from "./API_key.js";
 
 
 submitButton.addEventListener("click", (e) => {
     let city = inputField.value;
-    console.log(city);
-    const openWeatherURL =
-        "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + OW_API_KEY + "&units=metric";
+    cityOfChoice.innerHTML = city;
+    const openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" 
+                            + city + "&appid=" + OW_API_KEY + "&units=metric";
+
     fetch(openWeatherURL)
         .then((response) => response.json())
         .then((data) => {
             const temperature = Math.round(data.main.temp);
-            const temperatureInF = Math.round((temperature * 9/5) + 32);
+            const temperatureInF = Math.round((temperature * 9 / 5) + 32);
             const weather = data.weather[0].main;
             const weatherDescription = data.weather[0].description;
-
-            temperatureOfCity.innerHTML = temperature + "°C / " + temperatureInF + "°F";
-            weatherOfCity.innerHTML = weather;
-
             console.log(data);
-
-
-
+            temperatureOfCity.innerHTML = "Temperature: " + temperature + "°C / " + temperatureInF + "°F";
+            weatherOfCity.innerHTML = weather;
+            weatherDesc.innerHTML = weatherDescription;
         })
-
         .catch((error) => {
             console.log(error);
         });
+    
 });
