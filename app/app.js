fe0907d3961b56c9cd031cda647f190ce332cd71
mrperrytpx <tomislav.siprak1996.gmail.com>
2021-01-21 21:35:55 +0100

    LF fucking stylist

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const inputField = document.querySelector(".input-city");
-const submitButton = document.querySelector(".submit-button");
+const inputSection = document.querySelector(".input-section");
 const cityOfChoice = document.querySelector(".city-of-choice");
 const weatherOfCity = document.querySelector(".weather-of-city");
 const weatherDesc = document.querySelector(".weather-description");
@@ -7,27 +7,30 @@ const temperatureOfCity = document.querySelector(".temperature-of-city");
 
 import { OW_API_KEY } from "./API_key.js";
 
+inputField.addEventListener("keyup", (e) => {
+    if (e.keyCode === 13) {
+        inputField.classList.add("input-submitted");
+        inputSection.classList.add("vh");
 
-submitButton.addEventListener("click", (e) => {
-    let city = inputField.value;
-    cityOfChoice.innerHTML = city;
-    const openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" 
-                            + city + "&appid=" + OW_API_KEY + "&units=metric";
+        let city = inputField.value;
+        cityOfChoice.innerHTML = city;
+        const openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="
+            + city + "&appid=" + OW_API_KEY + "&units=metric";
 
-    fetch(openWeatherURL)
-        .then((response) => response.json())
-        .then((data) => {
-            const temperature = Math.round(data.main.temp);
-            const temperatureInF = Math.round((temperature * 9 / 5) + 32);
-            const weather = data.weather[0].main;
-            const weatherDescription = data.weather[0].description;
-            console.log(data);
-            temperatureOfCity.innerHTML = "Temperature: " + temperature + "°C / " + temperatureInF + "°F";
-            weatherOfCity.innerHTML = weather;
-            weatherDesc.innerHTML = weatherDescription;
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-    
+        fetch(openWeatherURL)
+            .then((response) => response.json())
+            .then((data) => {
+                const temperature = Math.round(data.main.temp);
+                const temperatureInF = Math.round((temperature * 9 / 5) + 32);
+                const weather = data.weather[0].main;
+                const weatherDescription = data.weather[0].description;
+                console.log(data);
+                temperatureOfCity.innerHTML = "Temperature: " + temperature + "°C / " + temperatureInF + "°F";
+                weatherOfCity.innerHTML = weather;
+                weatherDesc.innerHTML = weatherDescription;
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 });
