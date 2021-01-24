ec35d4b7acbbbf2676bb18770b4bbd096cf5f273
mrperrytpx <tomislav.siprak1996@gmail.com>
2021-01-24 03:18:16 +0100

    last one i swear

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ How can APIs be real if our eyes aren't
 
 Add your own api key in a file in the "app" folder with format:
 export const OW_API_KEY = "YOUR-KEY-IN-QUOTES";
+
+DONE FOR REAL THIS TIME
--- app/app.js
+++ app/app.js
@@ -8,40 +8,47 @@ const weatherDesc = document.querySelector(".weather-description");
 const humidity = document.querySelector(".humidity");
 const temperatureOfCity = document.querySelector(".temperature-of-city");
 
+// Importing OpenWeather API key from local file
 import { OW_API_KEY } from "./API_key.js";
 
+// Adding keyup event listener to the input
 inputField.addEventListener("keyup", (e) => {
     if (inputField.value === "") {
         inputSection.classList.remove("vh");
         inputField.classList.remove("input-submitted");
     }
 
-    if (e.keyCode === 13) {
+    if (e.keyCode === 13) { // Check if the user pressed the Enter key
         errorResponse.classList.remove("error-response");
         errorResponse.classList.add("wrong");
 
+        //Formatting the input so it looks right with multi word cities
         let inputCity = inputField.value.toLowerCase().split(" ");
         let city = [];
-        for (let i = 0; i < inputCity.length; i++) {
-            let adding = inputCity[i].charAt(0).toUpperCase() + inputCity[i].slice(1);
+        inputCity.forEach(element => {
+            let adding = element.charAt(0).toUpperCase() + element.slice(1);
             city.push(adding);
-        }
+        })
         city = city.join(" ");
 
         const openWeatherURL = "http://api.openweathermap.org/data/2.5/weather?q="
             + city + "&appid=" + OW_API_KEY + "&units=metric";
 
+        // Fetching the API request
         fetch(openWeatherURL)
-            .then(handleErrors)
-            .then((response) => response.json())
+            .then(handleErrors) // Check for errors before proceeding
+            .then((response) => response.json()) // Convert the response to JSON
             .then((data) => {
+
                 inputField.classList.add("input-submitted");
                 inputSection.classList.add("vh");
-                
+
+                // Get my local time (Croatia)
                 const date = new Date();
                 const hour = date.getHours();
                 const minute = date.getMinutes();
 
+                // Some API data
                 const country = data.sys.country;
                 const temperature = Math.round(data.main.temp);
                 const temperatureInF = Math.round((temperature * 9 / 5) + 32);
@@ -49,12 +56,24 @@ inputField.addEventListener("keyup", (e) => {
                 const weatherDescription = data.weather[0].description;
                 const timezone = (data.timezone) / 3600 - 1;
                 const humid = data.main.humidity;
-
+    
+                // Calculating proper time for different timezones
                 let number;
-                timezone < 0 ? number = hour - Math.abs(timezone)
-                    : timezone > 0 ? number = (hour + Math.abs(timezone)) - 24
-                        : number = hour;
+                if (timezone < 0) {
+                    number = hour - Math.abs(timezone);
+                    if (number < 0) {
+                        number += 24;
+                    }
+                } else if (timezone > 0) {
+                    number = (hour + Math.abs(timezone));
+                    if (number > 24) {
+                        number -= 24;
+                    }
+                } else {
+                    number = hour;
+                }
 
+                // Adding values to HTML elements
                 cityOfChoice.innerHTML = `${city}, ${country}`;
                 temperatureOfCity.innerHTML = `Temperature: ${temperature}°C / ${temperatureInF}°F.`;
                 weatherOfCity.innerHTML = weather;
@@ -63,12 +82,14 @@ inputField.addEventListener("keyup", (e) => {
                 humidity.innerHTML = `Humidity: ${humid}% humid.`;
             })
             .catch((error) => {
+                // If the input isn't valid, display an error
                 errorResponse.innerHTML = `Error: "${city}" Not Found. Try again!`
                 console.log(error);
             });
     }
 });
 
+//Stop fetch from resolving a promise even tho it errored out. Fetch is like a puppy
 function handleErrors(response) {
     if (!response.ok) {
         errorResponse.classList.add("error-response");
